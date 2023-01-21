60d730299a528c787e8e1c23997a4032f322fd8b
ThetisLake <68162082+ericpien@users.noreply.github.com>
2023-01-21 14:05:01 -0800

    adding first draft of schema.sql

---
+++ backend/schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS parking_spots;
+DROP TABLE IF EXISTS reservations;
+
+
+PRAGMA forein_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone_number INTEGER NOT NULL
+);
+
+CREATE TABLE parking_spots (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    lon REAL NOT NULL,
+    lat REAL NOT NULL,
+    address TEXT NOT NULL,
+    city TEXT NOT NULL,
+    country TEXT NOT NULL,
+    description TEXT NOT NULL,
+    owner_id INTEGER NOT NULL,
+    price REAL NOT NULL,
+    image_url TEXT NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE reservations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATETIME NOT NULL,
+    parking_id INTEGER NOT NULL,
+    reserver_id INTEGER NOT NULL,
+    owner_id INTEGER NOT NULL,
+    FOREIGN KEY (parking_id) REFERENCES parking_spots(id),
+    FOREIGN KEY (reserver_id) REFERENCES users(id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
