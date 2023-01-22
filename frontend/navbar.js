6b8b322b4716d878bd388a81949dc09bf5f4b47a
KwanF <ykfung66@hotmail.com>
2023-01-21 19:26:55 -0800

    Updated nav bar

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $("#navbar").addClass("navbar navbar-light bg-light")
-    $("#navbar").append('<a href="/frontend/">login</a>')
-    $("#navbar").append('<a href="/frontend/my_parking_stalls.html">My Parking Stalls</a>')
-    $("#navbar").append('<a href="/frontend/">logout</a>')
+    $("#navbar").append('<a href="/frontend/pages/booking.html">Home</a>')
+    $("#navbar").append('<a href="/frontend/pages/my_parking_stalls.html">Reservations</a>')
+    $("#navbar").append('<a href="/frontend/pages/add_parking.html">Offerings</a>')
+    $("#navbar").append('<a href="/frontend/index.html">Logout</a>')
 });
