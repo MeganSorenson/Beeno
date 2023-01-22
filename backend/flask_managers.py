5b791465925a4e93063919b3102b947bc47dd927
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2023-01-21 18:58:56 -0800

    /page and /book GET apis working

--- backend/flask_managers.py
+++ backend/flask_managers.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+import json
 
 
 class User_Manager:
@@ -68,17 +69,64 @@ class Park_Manager:
     def __init__(self, db):
         self.db = db
 
-    def get_all_parking_with(place, date):
-        # filters through database for ony prking stalls in the given place and
-        # goes through database and only returns those parking stalls that are not booked for the given date
+    def get_all_parking_with(self, place):
+        # filters through database for ony prking stalls in the given place
+        # place must be given as City,Country (no space)
         # returns a json with all those parking stalls that were filtered
-        pass
+        cur = self.db.cur
+
+        place = place.split(",")
+        city = place[0]
+        country = place[1]
+
+        rows = cur.execute(
+            "SELECT * from parking_spots WHERE city=? and country=?", (city, country,))
+        rows_dict = {}
+
+        for row in rows:
+            id = row[0]
+            data = {
+                "lon": row[1],
+                "lat": row[2],
+                "address": row[3],
+                "place": row[4] + ", " + row[5],
+                "description": row[6],
+                "price": row[7],
+                "owner_id": row[8],
+                "image_url": row[9]
+            }
+            rows_dict[id] = data
+
+        return json.dumps(rows_dict, indent=4)
 
     def get_parking_stall(self, parking_id):
         # checks if there is a parking_stall in the database with the given parking_id
         # returns a json of the parking with the given parking_id
         # or json status update with "status" that is "error" and "message" that is a description of the error
-        pass
+        cur = self.db.cur
+        cur.execute(
+            "SELECT * from parking_spots WHERE id=?", (parking_id,))
+
+        rows = cur.fetchall()
+
+        if len(rows) == 1:
+            row = rows[0]
+            id = row[0]
+            data = {
+                id: {
+                    "lon": row[1],
+                    "lat": row[2],
+                    "address": row[3],
+                    "place": row[4] + ", " + row[5],
+                    "description": row[6],
+                    "price": row[7],
+                    "owner_id": row[8],
+                    "image_url": row[9]
+                }
+            }
+            return json.dumps(data, indent=4)
+        else:
+            return jsonify(status="error", message="no parking stall with given id")
 
     def add_parking_stall(self, user_id, latitude, longitude, image_url, place, price, description):
         # returns a json object with "status" that is either "success" or "error"
