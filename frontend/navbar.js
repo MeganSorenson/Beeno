3398aff9e24fa0d4446ab81a5a132cd03a2ebab6
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2023-01-21 19:38:03 -0800

    starting work on signup page

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
     $("#navbar").addClass("navbar navbar-light bg-light")
-    $("#navbar").append('<a href="/frontend/">login</a>')
-    $("#navbar").append('<a href="/frontend/my_parking_stalls.html">My Parking Stalls</a>')
-    $("#navbar").append('<a href="/frontend/">logout</a>')
+    $("#navbar").append('<a href="/">login</a>')
+    $("#navbar").append('<a href="/pages/my_parking_stalls.html">My Parking Stalls</a>')
+    $("#navbar").append('<a href="/">logout</a>')
 });
