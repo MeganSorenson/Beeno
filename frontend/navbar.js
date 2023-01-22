b496c19cc189ec30f8edb5e88b8580a746688759
KwanF <ykfung66@hotmail.com>
2023-01-21 19:51:39 -0800

    change nav

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $("#navbar").addClass("navbar navbar-light bg-light")
-    $("#navbar").append('<a href="/frontend/pages/booking.html">Home</a>')
-    $("#navbar").append('<a href="/frontend/pages/my_parking_stalls.html">Reservations</a>')
-    $("#navbar").append('<a href="/frontend/pages/add_parking.html">Offerings</a>')
-    $("#navbar").append('<a href="/frontend/index.html">Logout</a>')
+    $("#navbar").append('<a href="/pages/booking.html">Home</a>')
+    $("#navbar").append('<a href="/pages/my_parking_stalls.html">Reservations</a>')
+    $("#navbar").append('<a href="/pages/add_parking.html">Offerings</a>')
+    $("#navbar").append('<a href="/index.html">Logout</a>').append(<span class="material-icons">&#xE87C;</span>)
 });
