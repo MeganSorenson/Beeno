15b01fd582e5b534c016d4c2c94b792ab4be5621
ThetisLake <68162082+ericpien@users.noreply.github.com>
2023-01-22 09:17:23 -0800

    Initial upload of map

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     console.log("ready!");
     $("#navbar").addClass("navbar navbar-light bg-light")
     $("#navbar").append('<a href="/pages/booking.html">Book A Stall</a>')
+    $("#navbar").append('<a href="/pages/map.html">Map</a>')
     $("#navbar").append('<a href="/pages/my_parking_stalls.html">My Bookings</a>')
     $("#navbar").append('<a href="/pages/add_parking.html">My Parking</a>')
     $("#navbar").append('<a href="/">Logout</a>')
---
+++ frontend/pages/map.js
@@ -0,0 +1,25 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXJpY3BpZW4iLCJhIjoiY2xkN2x3OWJqMW52azNucGF2MDhnZDM4eCJ9.6Os86rNwkmy35WNtZqpsZw';
+
+const monument = [-77.0353, 38.8895];
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: monument, // starting position [lng, lat]
+    zoom: 15 // starting zoom
+});
+
+const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Construction on the Washington Monument began in 1848.<a href=google.com>test</a>'
+);
+     
+// create DOM element for the marker
+const el = document.createElement('div');
+el.id = 'marker';
+    
+// create the marker
+new mapboxgl.Marker(el)
+.setLngLat(monument)
+.setPopup(popup) // sets a popup on this marker
+.addTo(map)
